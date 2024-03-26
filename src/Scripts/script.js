591109c87a625fb2c2c2963b4c46ccf22065f77a
Luqmaan <Luqmaan78600@gmail.com>
2024-03-26 12:06:23 +0200

    added introduction section and started on about me section

--- src/Scripts/script.js
+++ src/Scripts/script.js
@@ -1,1 +1,20 @@
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+let display_btn = document.getElementById('btn_toggle-content');
+let hidden_p = document.getElementById('hidden_content');
+function display_hidden_content(){
+    if(hidden_p.classList.contains('d-none')){
+        hidden_p.classList.remove('d-none');
+        hidden_p.classList.remove('slide-up');
+        hidden_p.classList.add('slide-down');
+    }
+    else{
+        hidden_p.classList.remove('slide-down');
+        hidden_p.classList.add('slide-up');
+        setTimeout(function() {
+            hidden_p.classList.add('d-none');
+        }, 200);
+    }
+   
+}
+
+display_btn.addEventListener('click', display_hidden_content);
